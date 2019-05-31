5673e75d079646e5d129821e11ffb9f692fbac2b
Edward Feng <feng.dingding@gmail.com>
2019-05-31 09:41:13 -0400

    Make prop component

---
+++ src/post/Post.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./post.css";
+
+function Post() {
+    return (
+        <div className="post">
+            <h1>{this.props.title}</h1>
+            <p>{this.props.body}</p>
+            <br />>
+            <h2>{this.prop.author}</h2>
+        </div>
+    );
+}
