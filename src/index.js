5a7525ad3fac7f7f2fec2aab516b6fea922540b6
jerkur22 <jeremykurian68@gmail.com>
2019-06-05 13:25:37 -0400

    Changed index

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const routing = (
                 </li>
             </ul>
             <Route exact path="/" component={App} />
+            <Route path="/newUser"component={App}/>
         </div>
     </Router>
 )
